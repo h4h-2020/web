ec4edad5c5e04c926982dca6bc4b4e239aed16b3
Yuya Yabe <yuyabee@gmail.com>
2020-02-22 15:41:29 -0800

    Done setting up db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  const r = await db.query('SELECT * FROM users')
+  console.log(r);
   res.render('index', { title: 'Express' });
 });
 
