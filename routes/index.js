6218715a13b28e3b23c90de4be1c5abba377e087
Yuya Yabe <yuyabee@gmail.com>
2020-02-23 03:57:39 -0800

    Done login/register/logout for users

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,46 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db') /* database wrapper */
 var bcrypt = require('bcrypt'); /* cryptography stuff */ 
-
+const passport = require('../auth') 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const r = await db.query('SELECT * FROM users')
   console.log(r);
-  res.render('index', { title: 'Boost' });
+  res.render('index', { title: 'Boost', user: req.user && !req.user.is_admin, admin: req.user && req.user.is_admin });
 });
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
-router.post('/register', function(req, res, next) {
-  // TODO: implement the functionality
-  // refer to routes/admins.js
-  // very similar but use users table instead of admins table
-  res.redirect('/')
+router.post('/register', async function(req, res, next) {
+  try {
+    var pwd = await bcrypt.hash(req.body.password, 5);
+    const user_id = await db.query('SELECT id FROM users WHERE "email"=$1 and "is_admin"=$2', [req.body.username, false])
+    if (user_id.rowCount > 0) {
+      console.log("this user exists")
+      req.flash('info', "This emailed is already registered. <a href='/login'>Log in!</a>")
+      res.redirect('/login');
+    } else {
+      console.log("this user doesn't exist")
+      const insert = await db.query('INSERT INTO users (email, password, is_admin) VALUES ($1, $2, $3)', [req.body.username, pwd, false]);
+
+      console.log(insert)
+
+      if (insert) {
+        console.log(insert)
+        req.flash('info','user created.')
+        res.redirect('/login');
+      } else {
+        req.flash('info', "Something went wrong");
+        res.redirect('/register');
+      }
+    }
+  } catch(e) {
+    console.log(e)
+    req.flash('info', "Something went wrong");
+    res.redirect('/register');
+  }
 });
 
 /* GET login page */
@@ -28,36 +51,25 @@ router.get('/login', function (req, res, next) {
 
 /* GET listings page */
 router.get('/listings', function (req, res, next) {
-  res.render('listings', { title: 'Listings'} );
-});
+  // pass different listings depending on the user's login condition
+  res.render('listings', { title: 'Listings', user: req.user && !req.user.is_admin } );
+  });
 
 /* GET listings-hard page */
 router.get('/listings-hard', function (req, res, next) {
-  res.render('listings-hard', { title: 'Listings' });
+  // pass different listings depending on the user's login condition
+  res.render('listings-hard', { title: 'Listings', user: req.user && !req.user.is_admin});
 });
 
 /* Modify router to handle the submit button and confirm in database */
-router.post('/login', async function(req, res, next) {
-  try {
-    const user = await db.query('SELECT id, password FROM "users" WHERE "email"=$1', [req.body.email])
+router.post('/login', passport.authenticate('userLocal', { failureRedirect: '/login' }), function(req, res, next) {
+  res.redirect('/listings');
+});
 
-    if (user.rowCount == 1) { /* Makes sure that the number of rows in the table that match the email are 1 (meaning email unique) */
-      var r = await bcrypt.compare(req.body.password, user.rows[0].password)
-      if (r) {
-        req.flash('info', "Logged successfully.");
-        res.redirect('/');
-      } else {
-        req.flash('info', "Login information incorrect");
-        res.redirect('/login');
-      }
-    } else {
-      req.flash('info', "Login information incorrect");
-      res.redirect('/login');
-    }
-  } catch(e) {
-    req.flash('info', "Login information incorrect");
-    res.redirect('/login');
-  }
+router.get('/logout', function(req, res){
+  req.logout();
+  req.flash('info', 'Your are logged out. See you soon!');
+  res.redirect('/');
 });
 
 module.exports = router;
